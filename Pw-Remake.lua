fe15337ebdbf3e43df6c2b513c0216c58d12760f
Twix <91284215+Twixks@users.noreply.github.com>
2022-02-20 14:11:54 +0400

    Update Pw-Remake.lua

--- Pw-Remake.lua
+++ Pw-Remake.lua
@@ -1,7 +1,4 @@
---[[    Pw remake
-    I probably update this EVERY single Day
-    What an Explosm tbh, Anyway i've made some Updates so enjoy !
-]]
+
 
 local GetService = setmetatable({}, {
     __index = function(self, key)
@@ -1022,13 +1019,17 @@ ESPSection:AddToggle('Tracer ESP', false, function(onoff)
     ESP.Tracers = onoff
 end)
 
+ESPSection:AddToggle('Highlight', false, function(onoff)
+    ESP.Highlighted = onoff
+end)
+
 local ESPSettings = VisualsTab:CreateSector("ESP Settings", "right")
 
 ESPSettings:AddSlider('Attach Shift', 1, 1, 10, 1, function(Value)
     ESP.AttachShift = Value
 end)
 
-ESPSettings:AddSlider('Thickness', 1, 2, 20, 1, function(Value)
+ESPSettings:AddSlider('Thickness', 1, 1.5, 20, 2, function(Value)
     ESP.Thickness = Value
 end)
 
@@ -1054,16 +1055,8 @@ LocalSection:AddSlider("FOV Changer", 70, 70, 120, 1, function(Value)
 	local InputFOV = (Value)local L_418_ = game.Players.LocalPlayer
 	game:GetService("Workspace").Camera.FieldOfView = (Value)
 	PuppywareSettings.Visuals.Local.FOVChanger = Value
+	
 end)
-
-LocalSection:AddSlider("Gravity", 1, 196.2, 600, 1, function(Value)
-    game.Workspace.Gravity = (Value)
-end)
-
-LocalSection:AddSlider("WalkSpeed", 1, 16, 900, 1, function(Value)
-    game.Character:FindFirstChildOfClass('Humanoid').WalkSpeed = Value
-end)
-
 LocalSection:AddButton("Headless", function()
     Notify({
         Title = "Puppyware",
@@ -1072,12 +1065,91 @@ LocalSection:AddButton("Headless", function()
     })
 end)
 
+LocalSection:AddButton("No Face", function()
+    game.Players.LocalPlayer.Character.Head.face:Destroy()
+end)
+
+LocalSection:AddButton("Hide Mask", function()
+    game.players.LocalPLayer.Character:FindFirstChild('In-gameMask').Handle:Destroy()
+end)
+
 LocalSection:AddButton("Korblox", function()
     game.Players.LocalPlayer.Character.RightUpperLeg:Destroy()
 end)
 
 LocalSection:AddButton("Hide Boombox", function()
-    player.Character.BOOMBOXHANDLE:Destroy()    -- i wonder why i made this
+    game.Players.LocalPlayer.Character.BOOMBOXHANDLE:Destroy()    -- i wonder why i made this
+end)
+
+LocalSection:AddButton("Chat Spy", function()
+--SCRIPT NOT MADE BY ME I JUST NEEDED A LOADSTRING FOR IT
+--\\ lol i was so humble 
+
+--chat "/spy" to toggle!
+enabled = true
+--if true will check your messages too
+spyOnMyself = true
+--if true will chat the logs publicly (fun, risky)
+public = false
+--if true will use /me to stand out
+publicItalics = true
+--customize private logs
+privateProperties = {
+	Color = Color3.fromRGB(0,255,255); 
+	Font = Enum.Font.SourceSansBold;
+	TextSize = 18;
+}
+--////////////////////////////////////////////////////////////////
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer or Players:GetPropertyChangedSignal("LocalPlayer"):Wait() or Players.LocalPlayer
+local saymsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+local getmsg = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("OnMessageDoneFiltering")
+local instance = (_G.chatSpyInstance or 0) + 1
+_G.chatSpyInstance = instance
+
+local function onChatted(p,msg)
+	if _G.chatSpyInstance == instance then
+		if p==player and msg:lower():sub(1,4)=="/spy" then
+			enabled = not enabled
+			wait(0.3)
+			privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+			StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+		elseif enabled and (spyOnMyself==true or p~=player) then
+			msg = msg:gsub("[\n\r]",''):gsub("\t",' '):gsub("[ ]+",' ')
+			local hidden = true
+			local conn = getmsg.OnClientEvent:Connect(function(packet,channel)
+				if packet.SpeakerUserId==p.UserId and packet.Message==msg:sub(#msg-#packet.Message+1) and (channel=="All" or (channel=="Team" and public==false and Players[packet.FromSpeaker].Team==player.Team)) then
+					hidden = false
+				end
+			end)
+			wait(1)
+			conn:Disconnect()
+			if hidden and enabled then
+				if public then
+					saymsg:FireServer((publicItalics and "/me " or '').."{SPY} [".. p.Name .."]: "..msg,"All")
+				else
+					privateProperties.Text = "{SPY} [".. p.Name .."]: "..msg
+					StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+				end
+			end
+		end
+	end
+end
+
+for _,p in ipairs(Players:GetPlayers()) do
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end
+Players.PlayerAdded:Connect(function(p)
+	p.Chatted:Connect(function(msg) onChatted(p,msg) end)
+end)
+
+privateProperties.Text = "{SPY "..(enabled and "EN" or "DIS").."ABLED}"
+StarterGui:SetCore("ChatMakeSystemMessage",privateProperties)
+if not player.PlayerGui:FindFirstChild("Chat") then wait(3) end
+local chatFrame = player.PlayerGui.Chat.Frame
+chatFrame.ChatChannelParentFrame.Visible = true
+chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
 end)
 
 --[[
@@ -1462,6 +1534,20 @@ MiscSector:AddButton('Body Reset', function(State)
     pcall(function()
         LocalPlayer.Character.Humanoid:Destroy()
     end)
+end)
+
+MiscSector:AddButton('Reset', function(State)
+    game.Players.LocalPlayer.Character.Head:Destroy()
+end)
+
+local ResetBind = MiscSector:AddKeybind("Rest Bind", false, function()
+end, function()
+    game.Players.LocalPlayer.Character.Head:Destroy()
+end)
+
+local BodyResetBind = MiscSector:AddKeybind("Body Reset Bind", false, function()
+end, function()
+    LocalPlayer.Character.Humanoid:Destroy()
 end)
 
 --[[
